1d2da0c0430fcae76c2e3dd69aac71c6e7fcce03
Andre Dalwin <andudtan@gmail.com>
2024-11-15 11:52:52 +0800

    Update LiSANet to support 3d

--- Model/lib/models/modules/LiSASEBlock.py
+++ Model/lib/models/modules/LiSASEBlock.py
@@ -1,20 +1,28 @@
+# LiSASEBlock.py
 import torch
 import torch.nn as nn
 
 class SEBlock(nn.Module):
     def __init__(self, channels, reduction=16, dim="2d"):
         super(SEBlock, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        if dim == "3d":
+            self.avg_pool = nn.AdaptiveAvgPool3d(1)
+            self.fc1 = nn.Conv3d(channels, channels // reduction, kernel_size=1, bias=False)
+            self.fc2 = nn.Conv3d(channels // reduction, channels, kernel_size=1, bias=False)
+        elif dim == "2d":
+            self.avg_pool = nn.AdaptiveAvgPool2d(1)
+            self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1, bias=False)
+            self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1, bias=False)
+        else:
+            raise ValueError(f"Invalid dimension '{dim}' for SEBlock")
 
-        self.fc = nn.Sequential(
-            nn.Linear(channels, channels // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channels // reduction, channels, bias=False),
-            nn.Sigmoid()
-        )
+        self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        b, c = x.size(0), x.size(1)
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, *([1] * (x.dim() - 2)))
-        return x * y.expand_as(x)
+        y = self.avg_pool(x)
+        y = self.fc1(y)
+        y = self.relu(y)
+        y = self.fc2(y)
+        y = self.sigmoid(y)
+        return x * y
